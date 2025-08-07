9e37b4fbf68a82fb190e79d49f365b3d2dad5e2b
kcwarsa <keise.sinclair@gmail.com>
2025-08-07 11:08:36 -0700

    Delete Habit Tracking Application directory

--- Habit Tracking Application/App.tsx
+++
@@ -1,9 +0,0 @@
-import { MobileHabitTracker } from "./components/MobileHabitTracker";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-background">
-      <MobileHabitTracker />
-    </div>
-  );
-}
